bce8593c61951e4a9de4ffd30a2a2f253378b5ca
nitazoso <nitalarasoo7@gmail.com>
2026-05-21 10:33:26 +0700

    cari

--- website/atmin/addAdmin.php
+++ website/atmin/addAdmin.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+   
+    </style>
 </head>
 <body>
     <form data-ajax="true" data-action="./process/pro_addAdmin.php" data-notif="notif">
